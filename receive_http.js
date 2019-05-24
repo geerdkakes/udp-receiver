850bbc107bc2dc5312409c2faa303e18b1d38f08
Geerd Kakes <geerd@kakes.net>
2019-05-24 10:24:53 +0200

    added tcp receive script

---
+++ receive_http.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var handleRequest = function(request, response) {
+    console.log("received connection request: " + request.connection.remoteAddress);
+    response.writeHead(200);
+    response.end("Hello NEW World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(5005);
